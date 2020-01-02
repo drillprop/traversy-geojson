bf54db0cd122b97de8a5f3152540ef6e5608e5d4
drillprops <drylbartosz@gmail.com>
2020-01-02 17:16:17 +0100

    create Store model and add it to Store controller

---
+++ models/Store.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const StoreSchema = new Schema({
+  storeId: {
+    type: String,
+    required: [true, 'Please add store id'],
+    unique: true,
+    trim: true,
+    maxlength: [10, 'Store ID must be less than 10 chars ']
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address']
+  },
+  location: {
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ['Point'] // 'location.type' must be 'Point'
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere'
+    },
+    formattedAddress: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = model('Store', StoreSchema);
