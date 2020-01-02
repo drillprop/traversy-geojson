ec870eb99b272084d757cc6e4adc8f184194f5be
drillprops <drylbartosz@gmail.com>
2020-01-02 18:32:26 +0100

    create addStore controller

--- controllers/stores.js
+++ controllers/stores.js
@@ -17,3 +17,24 @@ exports.getStores = async (req, res, next) => {
     res.status(500).json({ error: 'Server error' });
   }
 };
+
+// @desc  create a store
+// @route POST /api/v1/stores
+// @access public
+
+exports.addStore = async (req, res, next) => {
+  try {
+    const store = await Store.create(req.body);
+
+    return res.status(200).json({
+      success: true,
+      data: store
+    });
+  } catch (error) {
+    console.error(error);
+    if (error.code === 11000) {
+      return res.status(400).json({ error: 'This store already exists' });
+    }
+    res.status(500).json({ error: 'Server error' });
+  }
+};
